d21f4ad9a65e46e70c9b623cc82c7ff0069a51d7
britojulia <juliabri1234@gmail.com>
2025-05-13 21:41:35 -0300

    adicionando doc

--- Trak-In/src/main/java/br/com/fiap/Trak_In/TrakInApplication.java
+++ Trak-In/src/main/java/br/com/fiap/Trak_In/TrakInApplication.java
@@ -2,8 +2,18 @@ package br.com.fiap.Trak_In;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableCaching
+@OpenAPIDefinition(info = @Info(title = "TrackIn API", version = "1",
+description = "API do Challenge do para o projeto de java TrackIn ", 
+contact = @Contact(name = "Júlia Brito", 
+email = "juliabri1234@gmail.com")))
 public class TrakInApplication {
 
 	public static void main(String[] args) {
